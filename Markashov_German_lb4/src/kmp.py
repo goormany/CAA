c117f5a3ad5d6d0253df57519d2d84dc94f44e00
goormany <zarbot351@yandex.ru>
2026-05-12 02:55:28 +0300

    done stepik

---
+++ Markashov_German_lb4/src/kmp.py
@@ -0,0 +1,47 @@
+def get_prefix_func(s: str) -> list[int]:
+    n = len(s)
+    pi = [0] * n
+    
+    for i in range(1, n):
+        j = pi[i-1]
+        
+        while j > 0 and s[i] != s[j]:
+            j = pi[j-1]
+            
+        if s[j] == s[i]:
+            j += 1
+            
+        pi[i] = j
+    return pi
+
+def kmp(text: str, pattern: str) -> list[int]:
+    nt = len(text)
+    np = len(pattern)
+    if np == 0: return []
+    
+    pi = get_prefix_func(pattern)
+    res = []
+    j = 0
+    
+    for i in range(nt):
+        while j > 0 and text[i] != pattern[j]:
+            j = pi[j-1]
+        
+        if text[i] == pattern[j]:
+            j += 1
+        
+        if j == np:
+            res.append(i - np + 1)
+            j = pi[j-1]
+            
+    return res
+
+if __name__ == "__main__":
+    pattern = input()
+    text = input()
+    
+    result = kmp(text, pattern)
+    if result:
+        print(",".join(map(str, result)))
+    else:
+        print(-1)
